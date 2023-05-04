6fd7cc531083b7226e78efc46caed77ded34a3bb
Ching <89380886+Chingsan53@users.noreply.github.com>
2023-05-03 23:58:59 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('header.php'); ?>
+
+<main style="min-height: calc(100vh - 176px - 104px);">
+    <!-- Background image -->
+    <div class="relative overflow-hidden bg-no-repeat bg-cover" style="
+        background-position: 50%;
+        background-image: url('csuf.jpeg');
+        height: 350px;
+      ">
+        <div class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
+            style="background-color: rgba(0, 0, 0, 0.5)">
+            <div class="flex justify-center items-center h-full">
+                <div class="text-center text-white px-6 md:px-12 w-7/12">
+                    <h1 class="text-5xl font-bold mt-0 mb-6">California State University, Fullerton</h1>
+                    <h3 class="text-3xl font-semibold mb-8">IT TAKES A TITAN</h3>
+                    <h2 class="text-3xl font-semibold mb-8">THE CAMPAIGN FOR CAL STATE FULLERTON</h2>
+
+                    <button type="button"
+                        class="inline-block px-6 py-2.5 text-white bg-slate-900 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out"
+                        data-mdb-ripple="true" data-mdb-ripple-color="light">
+                        Get started
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
