fe3a71ba5f04ef0b5de0403b59d7c29ac9ede19f
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2023-05-05 13:49:27 -0700

    update index, added 2 pages, and update footer

--- index.php
+++ index.php
@@ -27,4 +27,22 @@
         </div>
     </div>
 
+    <div class="flex justify-center mb-10 space-x-20 mt-10 text-center">
+        <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+            <h5 class="text-gray-900 text-xl leading-tight font-medium mb-5">Professors</h5>
+            <img src="book.png" class="mx-auto w-32 mb-5" />
+            <button type="button" onclick="window.location.href = './professors.php'"
+                class="inline-block px-6 py-2.5 bg-slate-900 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out">Select</button>
+        </div>
+
+        <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+            <h5 class="text-gray-900 text-xl leading-tight font-medium mb-5">Students</h5>
+            <img src="student.png" class="mx-auto w-32 mb-5" />
+            <button type="button" onclick="window.location.href = './students.php'"
+                class="inline-block px-6 py-2.5 bg-slate-900 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out">Select</button>
+        </div>
+</div>
+
+
+
 <?php include('footer.php'); ?>
