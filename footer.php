e244de75e022974871fa43af8f7bd7c49eee2dcb
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2023-05-07 17:20:29 -0700

    update professor.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="text-center bg-gray-900 text-white">
-  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.2);">
+  <div class="text-center p-4" style="background-color: rgba(9, 35, 65, 0.2);">
     CPSC 332 Final Project</br>
-    <a class="text-white" href="https://tailwind-elements.com/">Contributors: Camplesg Nguyen, Chingsan Ly, and Enrique Aceves.
+    <a class="text-white" href="https://tailwind-elements.com/">Group Members: Camplesg Nguyen, Chingsan Ly, and Enrique Aceves.
       
   </div>
 </footer>
